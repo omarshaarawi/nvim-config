11e09a56fc6cdb9e2b91149bfcf2e6c2aa50d62c
Omar Shaarawi <omar.shaarawi@target.com>
2023-04-03 12:13:31 -0500

    better keybindings for autocomplete

--- lua/shaarawi/packer.lua
+++ lua/shaarawi/packer.lua
@@ -49,4 +49,15 @@ use {
         {"nvim-treesitter/nvim-treesitter"}
     }
 }
+use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+}
+use({
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	tag = "v<CurrentMajor>.*",
+	-- install jsregexp (optional!:).
+	run = "make install_jsregexp"
+})
 end)
